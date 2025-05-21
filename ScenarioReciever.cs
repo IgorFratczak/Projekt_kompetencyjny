ef403f25bb6096f82503981a1ac829e290122bf0
MichalC <m.colautti@o2.pl>
2025-05-21 15:42:15 +0200

    Added unity scenario reciever for unity

---
+++ ScenarioReciever.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+public class ScenarioReceiver : MonoBehaviour
+{
+    private HttpListener listener;
+    private CancellationTokenSource cts;
+
+    void Start()
+    {
+        cts = new CancellationTokenSource();
+        Task.Run(() => StartListener(cts.Token));
+    }
+
+    private async Task StartListener(CancellationToken token)
+    {
+        listener = new HttpListener();
+        listener.Prefixes.Add("http://*:5000/");
+        listener.Start();
+        Debug.Log("HTTP Listener started on port 5000");
+
+        while (!token.IsCancellationRequested)
+        {
+            var context = await listener.GetContextAsync();
+            var request = context.Request;
+
+            if (request.HttpMethod == "POST")
+            {
+                using (var reader = new System.IO.StreamReader(request.InputStream, request.ContentEncoding))
+                {
+                    string body = await reader.ReadToEndAsync();
+                    Debug.Log("Received scenario: " + body);
+                }
+
+                var response = context.Response;
+                string responseString = "OK";
+                byte[] buffer = Encoding.UTF8.GetBytes(responseString);
+                response.ContentLength64 = buffer.Length;
+                var output = response.OutputStream;
+                await output.WriteAsync(buffer, 0, buffer.Length);
+                output.Close();
+            }
+            else
+            {
+                context.Response.StatusCode = 405;
+                context.Response.Close();
+            }
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        cts.Cancel();
+        listener.Stop();
+        listener.Close();
+    }
+}
