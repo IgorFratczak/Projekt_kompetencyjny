579487ce92d06f6e5cb061e44200908e9afcd837
IgorFratczak <245804@edu.p.lodz.pl>
2025-05-21 14:25:20 +0200

    repair pin numbers

--- buttons_test.py
+++ buttons_test.py
@@ -1,11 +1,11 @@
 import RPi.GPIO as GPIO
 import time
 
-# Numery pinów GPIO
+# Numery pinów GPIO z nazwami i domyślnym stanem
 buttons = {
-    17: "Przycisk 1",
-    27: "Przycisk 2",
-    22: "Przycisk 3"
+    7: {'name': 'GPIO 4', 'state': GPIO.LOW},
+    18: {'name': 'GPIO 12', 'state': GPIO.LOW},
+    37: {'name': 'GPIO 23', 'state': GPIO.LOW}
 }
 
 # Ustawienie trybu numeracji pinów
@@ -19,10 +19,14 @@ print("Naciśnij przycisk (Ctrl+C aby zakończyć)")
 
 try:
     while True:
-        for pin, name in buttons.items():
+        for pin, data in buttons.items():
             if GPIO.input(pin) == GPIO.LOW:  # przycisk wciśnięty
-                print(f"{name} został naciśnięty")
-                time.sleep(0.3)  # opóźnienie, aby uniknąć wielokrotnego wykrycia
+                if data['state'] == GPIO.HIGH:
+                    continue  # pomiń, jeśli już wcześniej był wciśnięty
+                print(f"{data['name']} został naciśnięty")
+                data['state'] = GPIO.HIGH  # aktualizacja stanu
+            else:
+                data['state'] = GPIO.LOW  # przycisk nie wciśnięty
         time.sleep(0.1)
 
 except KeyboardInterrupt:
